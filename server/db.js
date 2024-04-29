d514f09b45f2364a87cb2ed6ec361e1e696d39a4
MahabaHubba <150589335+MahabaHubba@users.noreply.github.com>
2024-04-29 19:57:07 +1000

    Connection to db.js
    connection to mongodb database

--- server/db.js
+++ server/db.js
@@ -1,2 +1,10 @@
 require('dotenv').config();
 const mongoose = require('mongoose');
+
+
+mongoose
+    .connect(process.env.MONGO_CONNECTION_URL)
+    .then(() => {
+        console.log('Database connected succesfully')
+    })
+    .catch((error) => console.log('Error connecting to database'))
