56a3eb2369ea6e2ef9c3530fd2429f11ddeef0f4
MahabaHubba <150589335+MahabaHubba@users.noreply.github.com>
2024-04-30 18:10:38 +1000

    authMiddleWare and server.js
    Added authMiddleware and updated server.js

--- server/server.js
+++ server/server.js
@@ -1,41 +1,37 @@
+require('./db');
 const express = require('express');
 const { ApolloServer } = require('@apollo/server');
-const { expressMiddleware } = require('@apollo/server/express4');
-const path = require('path');
+const {typeDefs, resolvers} = require('./schemas/index');
+const {authMiddleware} = require('./middleware/authMiddleware')
 
-const { typeDefs, resolvers } = require('./schemas');
-const db = require('./db');
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3030;
 const app = express();
 const server = new ApolloServer({
   typeDefs,
   resolvers,
+  context: authMiddleware,
 });
 
-const startApolloServer = async () => {
-  await server.start();
-  
-  app.use(express.urlencoded({ extended: true }));
-  app.use(express.json());
-  
-  app.use('/graphql', expressMiddleware(server));
+(async () => {
+  try {
+    await server.start();
+    server.applyMiddleware({app});
+    app.use(express.urlencoded({ extended: false}));
+    app.use(express.json());
 
-  // if we're in production, serve client/dist as static assets
-  if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, '../client/dist')));
-
-    app.get('*', (req, res) => {
-      res.sendFile(path.join(__dirname, '../client/dist/index.html'));
-    });
-  } 
-
-  db.once('open', () => {
     app.listen(PORT, () => {
-      console.log(`API server running on port ${PORT}!`);
-      console.log(`Use GraphQL at http://localhost:${PORT}/graphql`);
+      console.log('Server started on port' + PORT);
+      console.log(
+        `GraphQL Server started on http://localhost:${server.graphqlPath}`
+      )
     });
-  });
-};
-
-startApolloServer();
+    app.use((error, req, res) => {
+      if(error) {
+        console.log('Err', error);
+      }
+    })
+  } catch (error) {
+    console.log(error)
+  }
+})();
