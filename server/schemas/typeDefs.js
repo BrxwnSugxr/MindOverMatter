90d378f169162991af5d3a36e47f20682a87755b
MahabaHubba <150589335+MahabaHubba@users.noreply.github.com>
2024-04-30 21:12:12 +1000

    Error fixed
    Error due to "EventInput being the wrong paramters"
    Also server connects properly

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,38 +1,41 @@
-const {gql} = require('apollo-server-express');
+const { gql } = require('apollo-server-express');
 
 const typeDefs = gql`
-    type User {
-        _id: ID!
-        username: String
-        email: String!
-    }
+  type User {
+    _id: ID!
+    username: String
+    email: String!
+  }
 
-    type Query {
-        getUser: User
-        getEvent: Event
-        getEvents: Event
-      
-    }
-    type Event {
-        _id: ID!
-        title: String!
-        description: String!
-        number_of_people: Int!
-        is_virtual: Boolean!
-    }
-    type AuthUser {
-        user: User!
-        token: String!
-    }
-    type Mutation {
-        register(username: String!, email: String!, password: String!):
-    User!
-        login(email: String!, password: String!): AuthUser!
-        createEvent(eventInput: EventInput!): Event
-        updateEvent(eventId: ID!, input: EventInput!): Event
-        deleteEvent(eventId: ID!): Event
-    }
+  type Query {
+    getUser: User
+    getEvent: Event
+    getEvents: Event
+  }
+  type Event {
+    _id: ID!
+    title: String!
+    description: String!
+    number_of_people: Int!
+    is_virtual: Boolean!
+  }
+  input EventInput {
+    title: String!
+    description: String!
+    number_of_people: String!
+    is_virtual: Boolean!
+  }
+  type AuthUser {
+    user: User!
+    token: String!
+  }
+  type Mutation {
+    register(username: String!, email: String!, password: String!): User!
+    login(email: String!, password: String!): AuthUser!
+    createEvent(eventInput: EventInput!): Event
+    updateEvent(eventId: ID!, input: EventInput!): Event
+    deleteEvent(eventId: ID!): Event
+  }
 `;
-
 
 module.exports = typeDefs;
--- server/server.js
+++ server/server.js
@@ -1,37 +1,38 @@
 require('./db');
 const express = require('express');
-const { ApolloServer } = require('@apollo/server');
-const {typeDefs, resolvers} = require('./schemas/index');
-const {authMiddleware} = require('./middleware/authMiddleware')
+const { ApolloServer } = require('apollo-server-express');
+const typeDefs = require('./schemas/typedefs');
+const resolvers = require('./schemas/resolvers');
+const { authMiddleware } = require('./middleware/authMiddleware');
 
-
-const PORT = process.env.PORT || 3030;
-const app = express();
 const server = new ApolloServer({
   typeDefs,
   resolvers,
   context: authMiddleware,
 });
+const app = express();
+const PORT = process.env.PORT || 3030;
 
 (async () => {
   try {
     await server.start();
-    server.applyMiddleware({app});
-    app.use(express.urlencoded({ extended: false}));
+    server.applyMiddleware({ app });
+    app.use(express.urlencoded({ extended: false }));
     app.use(express.json());
 
     app.listen(PORT, () => {
-      console.log('Server started on port' + PORT);
+      console.log('Server started on port ' + PORT);
       console.log(
         `GraphQL Server started on http://localhost:${server.graphqlPath}`
-      )
+      );
     });
+
     app.use((error, req, res) => {
-      if(error) {
+      if (error) {
         console.log('Err', error);
       }
-    })
+    });
   } catch (error) {
-    console.log(error)
+    console.log(error);
   }
 })();
