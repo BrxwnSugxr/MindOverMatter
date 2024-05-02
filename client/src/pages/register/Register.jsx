f1aa257367e3f6a1f88b535b0b732a3f9323880d
brxwnsugxr <jsdev.mas@gmail.com>
2024-05-02 20:23:00 +1000

    debugging: register pg, importing react state missing

--- client/src/pages/register/Register.jsx
+++ client/src/pages/register/Register.jsx
@@ -1,6 +1,6 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Register.css';
-import { Form } from 'react-bootstrap';
+import { Button, Form } from 'react-bootstrap';
 
 const Register = () => {
   return (
