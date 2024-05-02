aa8991770e85ec054174ac21e82c4a625d26400b
brxwnsugxr <jsdev.mas@gmail.com>
2024-05-02 20:28:09 +1000

    adding name to state handlechange

--- client/src/pages/register/Register.jsx
+++ client/src/pages/register/Register.jsx
@@ -3,21 +3,36 @@ import './Register.css';
 import { Button, Form } from 'react-bootstrap';
 
 const Register = () => {
+  const [state, setState] = useState({
+    username: '',
+    email: '',
+    password: '',
+  });
+
+  const handleChange = (event) => {};
   return (
     <div className="register">
       <h2 className="title">Register Page</h2>
       <Form className="register-form">
         <Form.Group className="mb-3" controlId="username">
           <Form.Label>Username</Form.Label>
-          <Form.Control type="text" placeholder="Enter Username" />
+          <Form.Control
+            type="text"
+            name="username"
+            placeholder="Enter Username"
+          />
         </Form.Group>
         <Form.Group className="mb-3" controlId="email">
           <Form.Label>Email</Form.Label>
-          <Form.Control type="email" placeholder="Enter Email" />
+          <Form.Control type="email" name="email" placeholder="Enter Email" />
         </Form.Group>
         <Form.Group className="mb-3" controlId="password">
           <Form.Label>Password</Form.Label>
-          <Form.Control type="password" placeholder="Enter password" />
+          <Form.Control
+            type="password"
+            name="password"
+            placeholder="Enter password"
+          />
         </Form.Group>
         <Button type="submit">Register</Button>
       </Form>
