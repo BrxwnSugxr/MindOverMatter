26d99f6c25bb81b88109853c3681e4a9f12ba810
brxwnsugxr <jsdev.mas@gmail.com>
2024-05-02 20:05:40 +1000

    routing page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
+import Login from './pages/login/login';
+import Register from './pages/register/Register';
+import Dashboard from './pages/dashboard/Dashboard';
 
 const App = () => {
-  return <div>App</div>;
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            isLoggedIn ? <Navigate to="/dashboard" /> : <Navigate to="/login" />
+          }
+        />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="*" element={<Navigate to="/" />} />
+      </Routes>
+    </BrowserRouter>
+  );
 };
 
 export default App;
