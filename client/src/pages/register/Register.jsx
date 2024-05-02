ef6ab73279a4d405d2c01ccd8f4ca2d49153606b
brxwnsugxr <jsdev.mas@gmail.com>
2024-05-02 20:15:39 +1000

    framework for register

--- client/src/pages/register/Register.jsx
+++ client/src/pages/register/Register.jsx
@@ -1,11 +1,26 @@
 import React from 'react';
-import './register.css';
+import './Register.css';
+import { Form } from 'react-bootstrap';
 
 const Register = () => {
   return (
     <div className="register">
       <h2>Register Page</h2>
-      <form className="register-form"></form>
+      <Form className="register-form">
+        <Form.Group className="mb-3" controlId="username">
+          <Form.Label>Username</Form.Label>
+          <Form.Control type="text" placeholder="Enter Username" />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="email">
+          <Form.Label>Email</Form.Label>
+          <Form.Control type="email" placeholder="Enter Email" />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="password">
+          <Form.Label>Password</Form.Label>
+          <Form.Control type="password" placeholder="Enter password" />
+        </Form.Group>
+        <Button type="submit">Register</Button>
+      </Form>
     </div>
   );
 };
