509260ddc677201ce16543021064057ba5672025
brxwnsugxr <jsdev.mas@gmail.com>
2024-05-04 14:34:21 +1000

    copy login change name and a few elements

--- client/src/pages/dashboard/Dashboard.jsx
+++ client/src/pages/dashboard/Dashboard.jsx
@@ -1,16 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Button } from 'react-bootstrap';
+import { useNavigate } from 'react-router-dom';
 
 const Dashboard = () => {
   const { data, loading } = userQuery(GET_USER);
   console.log('Dashboard data', data);
   const navigate = useNavigate();
   const [state, setState] = useState({
-    email: '',
-    password: '',
+    title: '',
+    description: '',
+    number_of_people: '',
+    isVirtual: false,
   });
 
-  const [loginUser, { error }] = useMutation(LOGIN_USER);
+  const [createEvent, { error }] = useMutation(CREATE_EVENT);
   const [successMsg, setSuccessMsg] = useState('');
 
   const handleChange = (event) => {
@@ -26,38 +29,60 @@ const Dashboard = () => {
     console.log(state);
     const { email, password } = state;
     if (email.trim() !== '' && password.trim() !== '') {
-      const { data } = await loginUser({
+      const { data } = await createEvent({
         data: {
           ...state,
         },
       });
       console.log('data', data);
-      locaclStorage.setItem('userToken', JSON.stringify(data.login.token));
-      setIsLoggedIn(true);
       setState({
-        email: '',
-        password: '',
+        title: '',
+        description: '',
+        number_of_people: '',
+        isVirtual: false,
       });
-      setSuccessMsg('Registration is successful');
+      setSuccessMsg('Event created successful');
       setTimeout(() => {
         setSuccessMsg('');
-        navigate('/dashboard');
       }, 2000);
     }
   };
 
   return (
-    <div>
-      <Form.Group className="mb-3" controlId="email">
-        <Form.Label>Email</Form.Label>
-        <Form.Control
-          type="email"
-          name="email"
-          value={state.email}
-          onChange={handleChange}
-          placeholder="Enter Email"
-        />
-      </Form.Group>
+    <div className="login">
+      <h2 className="title">Login Page</h2>
+      <Form className="login-form" onSubmit={handleSubmit}>
+        {successMsg && <p className="success-msg">{successMsg}</p>}
+        {error && (
+          <p className="error-msg">Something went wrong.Try again later</p>
+        )}
+
+        <Form.Group className="mb-3" controlId="title">
+          <Form.Label>title</Form.Label>
+          <Form.Control
+            type="title"
+            name="title"
+            placeholder="Enter title"
+            value={state.title}
+            onChange={handleChange}
+          />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="description">
+          <Form.Label>description</Form.Label>
+          <Form.Control
+            as="textarea"
+            rows={3}
+            description="description"
+            placeholder="Enter description"
+            value={state.description}
+            onChange={handleChange}
+          />
+        </Form.Group>
+        <Button type="submit">Register</Button>
+      </Form>
+      <div>
+        Don't have an account <Link to="/register">register here</Link>
+      </div>
     </div>
   );
 };
