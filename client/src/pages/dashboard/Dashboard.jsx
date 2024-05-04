e4980f64dcd38bd3cbc9b8eec8c6bdebd91380db
brxwnsugxr <jsdev.mas@gmail.com>
2024-05-04 14:09:56 +1000

    copy login

--- client/src/pages/dashboard/Dashboard.jsx
+++ client/src/pages/dashboard/Dashboard.jsx
@@ -1,9 +1,23 @@
 import React from 'react';
+import { Button } from 'react-bootstrap';
 
 const Dashboard = () => {
   const { data, loading } = userQuery(GET_USER);
   console.log('Dashboard data', data);
-  return <div>Dashboard</div>;
+  return (
+    <div>
+      <Form.Group className="mb-3" controlId="email">
+        <Form.Label>Email</Form.Label>
+        <Form.Control
+          type="email"
+          name="email"
+          value={state.email}
+          onChange={handleChange}
+          placeholder="Enter Email"
+        />
+      </Form.Group>
+    </div>
+  );
 };
 
 export default Dashboard;
