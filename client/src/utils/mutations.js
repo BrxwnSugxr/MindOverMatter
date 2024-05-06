27fea7a9d91060269f407bb1abb1fcf439e1f768
MahabaHubba <150589335+MahabaHubba@users.noreply.github.com>
2024-05-06 10:52:55 +1000

    update in mutation
    Added in registering admin, event

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -1,12 +1,33 @@
-//installation
 import { gql } from '@apollo/client';
 
 export const REGISTER_USER = gql`
-mutation register($username: String!, $email: String!, $password: String!) {
-    register(username: $username, email: $email, password: $password){
-        _id
-        username
-        email
+  mutation register($username: String!, $email: String!, $password: String!) {
+    register(username: $username, email: $email, password: $password) {
+      _id
+      username
+      email
     }
-}
-`
+  }
+`;
+
+export const REGISTER_ADMIN = gql`
+  mutation registerAdmin(
+    $username: String!
+    $email: String!
+    $password: String!
+  ) {
+    registerAdmin(username: $username, email: $email, password: $password) {
+      _id
+      username
+      email
+    }
+  }
+`;
+
+export const REGISTER_USER_FOR_EVENT = gql`
+  mutation registerUserForEvent($userId: String!, $eventId: String!) {
+    registerUserForEvent(userId: $userId, eventId: $eventId) {
+      success
+    }
+  }
+`;
